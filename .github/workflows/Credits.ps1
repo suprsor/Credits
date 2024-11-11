53b68bd7e6fcb9ed3bc05580f7889f6b347b4d35
suprsor <suprsors@gmail.com>
2024-11-11 01:27:34 -0800

    Create Credits.ps1

---
+++ .github/workflows/Credits.ps1
@@ -0,0 +1,11 @@
+$magenta = "Magenta"
+$red = "Red"
+$space = " " * 12  # Increased the number of spaces for more right alignment
+
+Write-Host "`n$space╭─────────────────────────────────────╮" -ForegroundColor $green
+Write-Host "$space│            SCAN COMPLETE            │" -ForegroundColor $green
+Write-Host "$space╰─────────────────────────────────────╯" -ForegroundColor $green
+
+Write-Host "$space╭─────────────────────────────────────╮" -ForegroundColor $blue
+Write-Host "$space│Discord @Suprsors | Twitter @Suprsors│" -ForegroundColor $blue
+Write-Host "$space╰─────────────────────────────────────╯" -ForegroundColor $blue
